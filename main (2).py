65dbc506c3b1d0ae97f26080a634cc2b7e0da12f
thoufeek93 <144427578+thoufeek93@users.noreply.github.com>
2023-09-19 11:30:59 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,7 @@
+#Online Python computer interpreters to Fun Python online.
+#write Python 3 code in this online editor and run it.
+year = 2020
+if(year%4 ==0):
+   print("leap year")
+else:
+    print("Not leap year")
